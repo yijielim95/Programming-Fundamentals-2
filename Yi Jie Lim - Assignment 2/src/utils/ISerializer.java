acf0d54a57553b98593185775fa6192806cfa3d5
Yi Jie Lim <166644831+yijielim95@users.noreply.github.com>
2024-04-25 17:11:25 +0100

    Delete Yi Jie Lim - Assignment 2 directory

--- Yi Jie Lim - Assignment 2/src/utils/ISerializer.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-// TODO Nothing!  This class is complete but you need to implement this in API classes
-public interface ISerializer {
-    void save() throws Exception;
-    void load() throws Exception;
-    String fileName();
-}
